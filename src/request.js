826136f13b89049e0ceefffc1e3cb491782e4e02
Cristi Constantin <cristi.constantin@sent.com>
2019-11-18 23:03:34 +0000

    Cache weather API calls

--- src/request.js
+++ src/request.js
@@ -1,17 +1,34 @@
 const r = require('r2')
+const cache = require('./cache')
 
-async function httpGet(_, {url, body = {}, headers = {}}, {double = false}) {
+async function httpGet(_, {url, headers = {}}, {double = false}) {
     if (url.slice(0, 4) !== 'http') {
         url = 'http://' + url
     }
 
-    const resp = await r(url, {json: body, headers}).text
-
-    if (double) {
-        return `\n${resp.trim()}\n`
+    const cached = await cache.getUrl(url)
+    if (cached && cached.text) {
+        console.log('Serve from cache::', cached.text)
+        return cached.text.trim()
     }
 
-    return resp.trim()
+    const resp = await r(url, {headers}).response
+    let text = await resp.text()
+    text = text.trim()
+    const ttl = 5 * 60 * 1000 // 5 minutes
+
+    if (resp.ok) {
+        console.log('Save in cache::', text)
+        await cache.cacheUrl(url, {text}, ttl)
+    } else {
+        console.error(text)
+    }
+
+    if (double) {
+        return `\n${text}\n`
+    }
+
+    return text
 }
 
 async function darkskyWeather(_, {latLong, units = null, lang = null, apiKey = null}, {double = false}) {
@@ -19,8 +36,7 @@ async function darkskyWeather(_, {latLong, units = null, lang = null, apiKey = n
      * Get Weather from api.darksky.net.
      * You need to register and provide your API KEY.
      */
-    const exclude = 'minutely,hourly,daily,alerts,flags'
-    apiKey = apiKey ? apiKey : process.env.DARKSKY_API_KEY
+    const exclude = 'minutely,hourly,daily,alerts'
 
     if (units) {
         units = `units=${units}&`
@@ -34,31 +50,77 @@ async function darkskyWeather(_, {latLong, units = null, lang = null, apiKey = n
         lang = ''
     }
 
-    const url = `https://api.darksky.net/forecast/${apiKey}/${latLong}?${units}exclude=${exclude}`
-    const resp = await r(url).text
+    const url = `https://api.darksky.net/forecast/$apiKey/${latLong}?${units}exclude=${exclude}`
 
-    if (double) {
-        return `\n${resp.trim()}\n`
+    const cached = await cache.getUrl(url)
+    if (cached && cached.text) {
+        console.log('Serve from cache::', url, cached.text)
+        return cached.text.trim()
     }
 
-    return resp.trim()
+    apiKey = apiKey ? apiKey : process.env.DARKSKY_API_KEY
+    const resp = await r(url.replace('$apiKey', apiKey)).response
+    const data = await resp.json()
+
+    units = data.flags.units === 'si' ? 'C' : 'F'
+    let text = `${data.currently.summary}, ${data.currently.temperature}° ${units}`
+    text += `, feels like ${data.currently.apparentTemperature}°`
+
+    const ttl = 10 * 60 * 1000 // 10 minutes
+    if (resp.ok) {
+        console.log('Save in cache::', url, text)
+        await cache.cacheUrl(url, {text}, ttl)
+    } else {
+        console.error(text)
+    }
+
+    if (double) {
+        return `\n${text}\n`
+    }
+
+    return text
 }
 
-async function openWeatherMap(_, {where, apiKey = null}, {double = false}) {
+async function openWeatherMap(_, {where, units = null, apiKey = null}, {double = false}) {
     /**
      * Get Weather from api.openweathermap.org.
      * You need to register and provide your API KEY.
      */
-    apiKey = apiKey ? apiKey : process.env.OPENWEATHER_API_KEY
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?APPID=${apiKey}&q=${where}`
-    const resp = await r(url).text
-
-    if (double) {
-        return `\n${resp.trim()}\n`
+    let unitQuery = ''
+    if (units) {
+        unitQuery = `&units=${units}`
     }
 
-    return resp.trim()
+    const url = `https://api.openweathermap.org/data/2.5/weather?APPID=$apiKey&q=${where}${unitQuery}`
+
+    const cached = await cache.getUrl(url)
+    if (cached && cached.text) {
+        console.log('Serve from cache::', cached.text)
+        return cached.text.trim()
+    }
+
+    apiKey = apiKey ? apiKey : process.env.OPENWEATHER_API_KEY
+    const resp = await r(url.replace('$apiKey', apiKey)).response
+    const data = await resp.json()
+
+    units = units === 'metric' ? 'C' : 'F'
+    let text = `${data.weather[0].description}, ${data.main.temp}° ${units}`
+    text += `, humidity ${data.main.humidity}%`
+    text = text.charAt(0).toUpperCase() + text.substr(1)
+
+    const ttl = 10 * 60 * 1000 // 10 minutes
+    if (resp.ok) {
+        console.log('Save in cache::', text)
+        await cache.cacheUrl(url, {text}, ttl)
+    } else {
+        console.error(text)
+    }
+
+    if (double) {
+        return `\n${text}\n`
+    }
+
+    return text
 }
 
 module.exports = {
